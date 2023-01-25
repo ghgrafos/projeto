feedb23ac5896d4a128aa1bd40ff9038b9721f6e
Gilmar Pimenta <grafos@outlook.com.br>
2023-01-24 23:45:49 -0300

    initial commit

---
+++ src/main/java/com/vmx/projeto/repository/PessoaRepository.java
@@ -0,0 +1,18 @@
+package com.vmx.projeto.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.vmx.projeto.model.Pessoa;
+
+@Repository
+@Transactional
+public interface PessoaRepository extends CrudRepository<Pessoa, Long> {
+	
+	@Query("select p from Pessoa p where p.nome like %?1%")
+	List<Pessoa> findPessoaByName(String nome);
+}
